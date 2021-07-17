b87533015d64c73b3dad0fac65548c7d012231d6
mam81 <mam8@cin.ufpe.br>
2021-07-17 10:56:22 -0300

    Aula01 - Implementação do header,card de perfil,feed e pessoas da comunidade

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,63 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { DevkutMenu, OrkutNostalgicIconSet } from '../src/lib/DevkutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelationsArea';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar(props) {
+  // console.log(props);
+  return (
+    <Box>
+      <img src={`https:github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'mam81';
+  const favoritesPersons = [
+    {name: 'Vitor Mourato', user: 'vitorlms'}, 
+    {name: 'RafaelDias', user: 'RafaelNAIP'}, 
+    {name: 'Milton Souto Maior', user:'BigMilts'},
+];
+
+  return (
+    <>
+    <DevkutMenu />
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <ProfileSideBar githubUser={githubUser} />
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box >
+          <h1 className="title"> 
+            Bem Vindo
+          </h1>
+
+          <OrkutNostalgicIconSet />
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <ProfileRelationsBoxWrapper >
+          <h2 className="smallTitle">
+            Pessoas da Comunidade ({favoritesPersons.length})
+          </h2>
+
+          <ul>
+            {favoritesPersons.map(person => {
+              return (
+                <li key={person.user}>
+                  <a href={`https://github.com/${person.user}`} >
+                    <img src={`https://github.com/${person.user}.png`} style={{ height: '100px' }} />
+                    <span>{person.name}</span>
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </ProfileRelationsBoxWrapper>
+
+      </div>
+    </MainGrid>
+    </>
+  )
 }
---
+++ src/components/MainGrid/index.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const MainGrid = styled.main`
+  width: 100%;
+  grid-gap: 10px;
+  padding: 16px;
+  margin-right: auto;
+  margin-left: auto;
+  max-width: 500px;
+  .profileArea {
+    display: none;
+    @media(min-width: 860px) {
+      display: block;
+    }
+  }
+
+  @media(min-width: 860px) {
+    display: grid;
+    max-width: 1100px;
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px; 
+  }
+`;
+
+export default MainGrid;
